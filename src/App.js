8d6c7cd9c230eebb0a5e1ab98a8f7382fe3ede33
Anthony Ngonde <anto.ngonde@gmail.com>
2024-04-28 12:15:20 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TransactionTable from './components/TransactionTable'
+
+
+
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+       <h1>The Royal Bank of Flatiron</h1>
+       <TransactionTable />
+     </div>
+  )
 }
 
 export default App;
+
+
+
+
